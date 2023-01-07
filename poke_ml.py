5dec80f06d25867eee7347642040cc363efde12d
Keegan Liu <keeganliu5678@gmail.com>
2023-01-07 00:44:26 -0500

    Begin implementing get_images logic

--- poke_ml.py
+++ poke_ml.py
@@ -51,5 +51,18 @@ conditions = [poke_df['Type2'] == '', poke_df['Type2'] != '']
 # Second element contains both types (if applicable) separated by '|'
 values = [poke_df['Type1'], poke_df['Type1'] + '|' + poke_df['Type2']]
 
-
-
+def get_images(gen_folder_path: Path):
+  pre_gen5_poke_df = poke_df[poke_df['Num'] < 650] # Filter Pokemon from gen5 and earlier
+  pre_gen5_poke_df = pre_gen5_poke_df.set_index('Num')
+  
+  png_files = glob.glob(str(gen_folder_path) + "/**/*.png", recursive=True) # Generate list of png file path names
+  
+  # Remove invalid pokemon entries
+  png_filtered = [file for file in png_files if Path(file).stem.isdigit()]
+  png_filtered = [file for file in png_filtered if int(Path(file).stem) in list(pre_gen5_poke_df['Num'])]
+  
+  # Create new dataframe from paths to Pokemon images
+  gen_poke_df = pd.DataFrame(png_filtered)
+  # Add types column to new df using pre_gen5_poke_df
+  gen_poke_df['Types'] = [pre_gen5_poke_df.loc[int(Path(file).stem), 'Types'] for file in png_filtered]
+  # 102 doesn't exist in pre_gen5_pokedf eggsecute but exists in png_files?!
